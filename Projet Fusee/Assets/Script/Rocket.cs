058abdd39d36a7d424874a71bb7f8c8ea6c290bd
B533P-20\533 <abc@hotmail.com>
2018-08-24 17:01:52 -0400

    Projet fusee Cours 1

---
+++ Projet Fusee/Assets/Script/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    public Rigidbody RocketRigidBody;
+    [SerializeField] public float ForceUpward = 200;
+    [SerializeField] public float ForceSideWay = 200;
+    // Use this for initialization
+    void Start () {
+        RocketRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FlightInputManeger();
+        HorizontalInputManager();
+	}
+
+    private void HorizontalInputManager()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            RocketRigidBody.AddForceAtPosition(new Vector3(ForceSideWay, 0, 0),new Vector3(),new ForceMode());
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            RocketRigidBody.AddForceAtPosition(new Vector3(-ForceSideWay, 0, 0), new Vector3(),new ForceMode());
+        }
+    }
+
+    private void FlightInputManeger()
+    {
+        if (Input.GetAxis("Jump")>0)
+        {
+            RocketRigidBody.AddRelativeForce(0, ForceUpward, 0);
+        }
+    }
+}
